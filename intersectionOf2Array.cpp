039a5fd8812a8a3d9e7d045fd7bd28f3624571ca
Sumit Maurya <mauryasumit01@gmail.com>
2025-06-02 18:50:32 +0530

    optimise by removing visited array

--- intersectionOf2Array.cpp
+++ intersectionOf2Array.cpp
@@ -5,11 +5,11 @@ void intersectionArray(const vector<int>&arr1,const vector<int>&arr2,vector<int>
     int n=arr1.size();
     int m=arr2.size();
     int i=0 , j=0;
-    vector<int>vis1(n,0);
-    vector<int>vis2(m,0);
+    // vector<int>vis1(n,0);
+    // vector<int>vis2(m,0);
     while(i<n && j<m){
-        if(arr1[i] == arr2[j] && (!vis1[i]) && (!vis2[j]) ){
-            vis1[i]=1,vis2[j]=1;
+        if(arr1[i] == arr2[j]){
+            //vis1[i]=1,vis2[j]=1;
             ans.push_back(arr1[i]);
             i++; j++;
         }
